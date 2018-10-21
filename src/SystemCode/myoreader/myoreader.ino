07326395e47bcfe29321d048290f4736beb06c2c
Hector Dominguez <hdomingu@umich.edu>
2018-10-21 14:21:40 -0400

    Now initializes channels by averaging data

--- src/SystemCode/myoreader/myoreader.ino
+++ src/SystemCode/myoreader/myoreader.ino
@@ -27,7 +27,7 @@ FilterOnePole lowpassFilter2( LOWPASS, filterFrequency );
 #define MIDDLEIN A2
 #define RINGPINKYIN A3
 
-#define INITBUTTON 1
+#define INITBUTTON 2
 
 /*
  * My propositon:
@@ -116,14 +116,13 @@ void InitializeSensor(MyoSensor * thisSensor, int channelNumber)
 {
   thisSensor->channelNumber = channelNumber;
   pinMode(channelNumber, INPUT);
-  int yRest;
-  int yComfortable;
-  String toPrint = "";
-  String catString = "";
-  toPrint = "Calculating yRest for channel " + channelNumber;
-  Serial.println();
+  long yRest;
+  long yComfortable;
+
+
+  Serial.println("Calculating yRest:");
   //calculate yRest (5 seconds rest)
-  int yAccumulation = 0;
+  long yAccumulation = 0;
   int count = 0;
   while(count < 5000)
   {
@@ -131,14 +130,14 @@ void InitializeSensor(MyoSensor * thisSensor, int channelNumber)
     delay(1);
     count++;
   }
+  Serial.println(yAccumulation);
   yRest = yAccumulation / count;
-  toPrint = "yRest(" + channelNumber;
-  catString = + ") = " + yRest;
-  toPrint = toPrint + catString;
-  Serial.println(toPrint);
+//  toPrint = "yRest(" + channelNumber;
+//  catString =") = " + yRest;
+//  toPrint = toPrint + catString;
+  Serial.println(yRest);
 
-  toPrint = "Calculating yComfortable for channel " + channelNumber;
-  Serial.println(toPrint);
+  Serial.println("Calculating yComfortable:");
   //calculate yComfortable (5 seconds comfortable contraction)
   yAccumulation = 0;
   count = 0;
@@ -148,15 +147,14 @@ void InitializeSensor(MyoSensor * thisSensor, int channelNumber)
     delay(1);
     count++;
   }
+  Serial.println(yAccumulation);
   yComfortable = yAccumulation / count;
-  toPrint = "yComfortable(" + channelNumber;
-  catString = + ") = " + yComfortable;
-  toPrint = toPrint + catString;
-  Serial.println(toPrint);
+  Serial.println(yComfortable);
 
   thisSensor->Yc = yComfortable;
   thisSensor->Yr = yRest;
-  return;
+   return;
+ 
 }
 
 //--------------------------- WHOLE HAND--------------//
@@ -173,15 +171,20 @@ MyoHand Hand;
 
 void InitializeFingers(void)
 {
-  InitializeSensor(&Hand.thumb,THUMBIN);
+//  Serial.println("-------------Thumb");
+//  InitializeSensor(&Hand.thumb,THUMBIN);
+  Serial.println("-------------Index");
   InitializeSensor(&Hand.index,INDEXIN);
-  InitializeSensor(&Hand.middle,MIDDLEIN);
-  InitializeSensor(&Hand.ringPinky,RINGPINKYIN);
+//  Serial.println("-------------Middle");
+//  InitializeSensor(&Hand.middle,MIDDLEIN);
+//  Serial.println("-------------RingPinky");
+//  InitializeSensor(&Hand.ringPinky,RINGPINKYIN);
 }
 
 /* ------------------------------------------- Myoelectric Interface ------------------------------------------------------------------------------------*/
 void setup() {
-
+  
+  pinMode(INITBUTTON,INPUT);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
   }
@@ -204,7 +207,15 @@ void setup() {
     ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
     ,  NULL );
 
-    while(!digitalRead(INITBUTTON));
+    while(1)
+    {
+      bool on = digitalRead(INITBUTTON);
+      Serial.println(on);
+      if(on)
+      {
+        break;
+      }
+    }
     InitializeFingers();
 
   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
